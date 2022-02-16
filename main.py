9041081728ee026d9e7bea9e442d830e6c008a95
eskil <eskil.ahlin@gmail.com>
2022-02-16 18:53:25 +0100

    ändringgar från hemdator

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 # nu är det på riktigt!
 import random as random
-
 import numpy as np
 
 pfield = np.array([[1, 0, 1], [1, 0, 1]])
 
 print(pfield)
 
+
+
 firstlife = random.randint(0,1)
-
-
 print(firstlife)
