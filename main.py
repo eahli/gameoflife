4b57b930f0c817d20b65e547373c2d8d011accea
eskil <eskil.ahlin@gmail.com>
2022-02-16 15:33:31 +0100

    lade till random och randint

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 # nu är det på riktigt!
+import random as random
 
 import numpy as np
 
 pfield = np.array([[1, 0, 1], [1, 0, 1]])
 
 print(pfield)
+
+firstlife = random.randint(0,1)
+
+
+print(firstlife)
