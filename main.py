57ad796bba20920cab933d4077683ea31fb154d7
eskil <eskil.ahlin@gmail.com>
2022-02-17 10:00:48 +0100

    ändringar torsdag jobb

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 # nu är det på riktigt!
 # detta är ändringar från jobdator torsdag
 import random as random
-import numpy as numpy
+import numpy as np
 
-pfield = numpy.array([[0, 0, 0], [0, 0, 0], [0,0,0]])
+#detta definerar storleken på spelfältet, tänk på att det börjar med 0
+pfieldx = 3
+pfieldy = 3
 
+pfield = np.matrix([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
 
 print(pfield)
 
 print(' \n och nu lägger vi till random 0 och 1  \n')
 
-firstlife = random.randint(0,1)
-print(firstlife)
+for x in range(0,pfieldx):
+    for y in range(0,pfieldy):
+        pfield[x,y]=random.randint(0,1)
+
+print(pfield)
+
