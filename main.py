4772a687520a8bbb4de7050f91c4bd64749b7085
eskil <eskil.ahlin@gmail.com>
2022-02-17 09:18:56 +0100

    ändringar torsdag jobb

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # nu är det på riktigt!
 # detta är ändringar från jobdator torsdag
 import random as random
-import numpy as np
+import numpy as numpy
 
-pfield = np.array([[1, 0, 1], [1, 0, 1]])
+pfield = numpy.array([[0, 0, 0], [0, 0, 0], [0,0,0]])
+
 
 print(pfield)
 
-
+print(' \n och nu lägger vi till random 0 och 1  \n')
 
 firstlife = random.randint(0,1)
 print(firstlife)
